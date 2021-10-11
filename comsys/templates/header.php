4ae08f36eea894612e3194bcb90e9c5e3d701af9
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-10-11 11:55:59 -0400

    sacado modulo de comercial

--- comsys/templates/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <title>Control de la Produccion</title>
-  <link rel="stylesheet" href="../res/css/semantic.min.css">
-  <link rel="stylesheet" href="../res/css/icon.min.css">
-</head>
